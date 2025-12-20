7f2b82b15e6b5a0993d124c135b2572ba76ddc3f
redzunder <>
2025-12-20 22:36:42 +0100

    Working interface, only need to fix UR5 command mode

--- README.md
+++ README.md
@@ -3,7 +3,26 @@ This project consists in developing a CLI interface for basic control of a robot
 
 The `install_pks.sh` file will install required packages when executed.
 
+## Instructions
 
+```
+install ROS distro (Humble for Ubuntu 22 or Jazzy for Ubuntu 24)
+install docker
+colcon build
+source /opt/ros/{humble OR jazzy}/setup.bash
+source install/setup.bash
+download ur3 driver
+download packages to ros2_ws/src/
+install with rosdep
+colcon build
+source install/setup.bash
+
+ros2 run ur_robot_driver start_ursim.sh -m ur3
+ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur3 robot_ip:=192.168.56.101 launch_rviz:=true
+
+run usb_cam
+run camera_driver
+```
 
 
 
--- ros2_ws/src/camera_driver/camera_driver/camera_node.py
+++ ros2_ws/src/camera_driver/camera_driver/camera_node.py
@@ -1,54 +1,83 @@
 #!/usr/bin/env python3
 
-import rclpy                            # Python Client Library for ROS 2
-from rclpy.node import Node             # Handles the creation of nodes
-from sensor_msgs.msg import Image       # Image is the message type
-from geometry_msgs.msg import Point     #Point message
-from cv_bridge import CvBridge          # ROS2 package to convert between ROS and OpenCV Images
-import cv2                              # Python OpenCV library
+import rclpy                            #----------Python Client Library for ROS 2
+
+#----------Topic messages
+from builtin_interfaces.msg import Duration
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+
+#----------Others
+from cv_bridge import CvBridge          #----------ROS2 package to convert between ROS and OpenCV Images
+import cv2                              #----------Python OpenCV library
 import numpy as np
 
+SCREEN_SIZE=(620,720,3)
 
-class MinimalSubscriber(Node):
+class MinimalSubsPubs(Node):
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('minimal_subspubs')
 
         #----------SUBSCRIBER
-        self.declare_parameter('square_size',200)
-        self.rect_size=self.get_parameter('square_size').value
-        print(f"self.rect_size: {self.rect_size}")
         self.window_name = "camera"
         self.subscription = self.create_subscription(Image,'image_raw',self.listener_callback,10)
-        self.subscription           # --------------------- prevent unused variable warning
-        self.point = None
-        #----------PUBLISHER
-        self.publisher_ = self.create_publisher(Point, 'points_topic', 10)
-        timer_period = 0.5          # --------------------- in seconds
-        #self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.subscription               #----------prevent unused variable warning
 
+        #Trajectory points
+        self.test_traj = {"points":[[0.0, 3.0, 0.0, -2.0, 0.0, 0.0]],
+                          "time":[Duration(sec=5)]}
+
+        #----------PUBLISHER
+        self.UR_publisher = self.create_publisher(JointTrajectory, '/scaled_joint_trajectory_controller/joint_trajectory', 10)
+        self.declare_parameter('rect_size', 200)
+        self.rect_size = self.get_parameter('rect_size').value
+        print(f"self.rect_size: {self.rect_size}")
+        self.point = None
+        self.timer = None
+
+    ## Draw a square when clicking on the black window and publish data to UR5
     def listener_callback(self, image_data):
-        cv_image = np.zeros((512,700,3), np.uint8)
+        cv_image = np.zeros(SCREEN_SIZE, np.uint8)
+
+        cv2.line(cv_image, (0, (int)(SCREEN_SIZE[0] / 2)),                  #----------Divider line
+                 (SCREEN_SIZE[1], int(SCREEN_SIZE[0] / 2)), (0, 0, 255), 3)
+
         if(self.point is not None):
-            cv2.rectangle(cv_image,self.point,(self.point[0]+self.rect_size,self.point[1]+self.rect_size),(0,150,200),3)
+            cv2.rectangle(cv_image,self.point,(self.point[0]+self.rect_size,self.point[1]+self.rect_size),(0,250,0),3)
+
         cv2.imshow(self.window_name,cv_image)
         cv2.waitKey(25)
-        cv2.setMouseCallback(self.window_name, self.draw_rectangle)
+        cv2.setMouseCallback(self.window_name, self.trajectory_to_robot)
 
-    def draw_rectangle(self, event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:  # --------------------- check if mouse event is clicked
-            self.point = (x,y)
-    
-    def timer_callback(self):
-        msg = Point()
-        msg.x=0
-        msg.y=0
-        msg.z=0
-        self.publisher_.publish(msg)
+    ## Create and publish data to UR5
+    def trajectory_to_robot(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:                      #----------check if mouse event is clicked
+            self.point = (x, y)                                 #----------to draw rectangle
+
+            #Create suitable topic message
+            trajectory = JointTrajectory()
+
+            trajectory.joint_names = ["shoulder_pan_joint","shoulder_lift_joint","elbow_joint",
+                                "wrist_1_joint","wrist_2_joint","wrist_3_joint"]
+            trajectory.points = [
+                JointTrajectoryPoint(
+                    positions=self.test_traj["points"][i], time_from_start=self.test_traj["time"][i]
+                )
+                for i in range(len(self.test_traj["points"]))
+            ]
+
+            print(trajectory)                                   #----------for debug
+            self.UR_publisher.publish(trajectory)
+
+
 
 
 def main(args=None):
     rclpy.init(args=args)
-    minimal_subscriber = MinimalSubscriber()
+    minimal_subscriber = MinimalSubsPubs()
     rclpy.spin(minimal_subscriber)
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
