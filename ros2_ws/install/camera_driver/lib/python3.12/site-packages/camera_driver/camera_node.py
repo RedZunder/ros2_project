0124acdd1c2fe24785e72feb9fbc0fcf9283fcd6
redzunder <>
2026-01-08 16:29:35 +0100

    Working UR control

--- ros2_ws/install/camera_driver/lib/python3.12/site-packages/camera_driver/camera_node.py
+++ ros2_ws/install/camera_driver/lib/python3.12/site-packages/camera_driver/camera_node.py
@@ -8,6 +8,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from trajectory_msgs.msg import JointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
+from control_msgs.msg import JointTrajectoryControllerState
 
 #----------Others
 from cv_bridge import CvBridge          #----------ROS2 package to convert between ROS and OpenCV Images
@@ -15,6 +16,11 @@ import cv2                              #----------Python OpenCV library
 import numpy as np
 
 SCREEN_SIZE=(620,720,3)
+def sum_arrays(arr1,arr2)->list[float]:
+    sum=[]
+    for i in range(len(arr1)):
+        sum.append(arr2[i]+arr1[i])
+    return sum
 
 class RobotControlNode(Node):
     def __init__(self):
@@ -23,7 +29,10 @@ class RobotControlNode(Node):
         #----------SUBSCRIBER
         self.window_name = "controller interface"
         self.subscription = self.create_subscription(Image,'image_raw',self.listener_callback,10)
+        self.ur_sub=self.create_subscription(JointTrajectoryControllerState,
+                             '/scaled_joint_trajectory_controller/controller_state',self.position_state_callback,10)
         self.subscription               #----------prevent unused variable warning
+        self.ur_sub
 
         #----------PUBLISHER
         self.UR_publisher = self.create_publisher(JointTrajectory, '/scaled_joint_trajectory_controller/joint_trajectory', 10)
@@ -47,6 +56,9 @@ class RobotControlNode(Node):
         cv2.waitKey(25)
         cv2.setMouseCallback(self.window_name, self.trajectory_to_robot)
 
+    def position_state_callback(self,state):
+        self.positions=state.output.positions
+
     ## Create and publish data to UR5
     def trajectory_to_robot(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:                      #----------check if mouse event is clicked
@@ -55,13 +67,14 @@ class RobotControlNode(Node):
             #Create suitable topic message
             trajectory = JointTrajectory()
             points=[[]]
-            time=[Duration(sec=5)]
+            time=[Duration(sec=1)]
 
             if y > SCREEN_SIZE[0]/2 :
-                points= [[0.0,-10.0, 0.0, 0.0, 0.0, 0.0]]
+                points= [sum_arrays(self.positions,[-0.8,0.0, 0.0, 0.0, 0.0, 0.0])]
             else :
-                points= [[0.0, 10.0, 0.0, 0.0, 0.0, 0.0]]
+                points= [sum_arrays(self.positions,[0.8,0.0, 0.0, 0.0, 0.0, 0.0])]
 
+            print(points)
             trajectory.joint_names = ["shoulder_pan_joint","shoulder_lift_joint","elbow_joint",
                                 "wrist_1_joint","wrist_2_joint","wrist_3_joint"]
             trajectory.points = [
